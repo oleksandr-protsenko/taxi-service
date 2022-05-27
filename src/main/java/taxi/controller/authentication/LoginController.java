5d1377150e7303ceaae2cd9ae3277e375f314666
Oleksander <ghjwtyrj25@gmail.com>
2022-05-27 17:17:55 +0300

    added checkstyle and changed mapping

--- src/main/java/taxi/controller/authentication/LoginController.java
+++ src/main/java/taxi/controller/authentication/LoginController.java
@@ -3,7 +3,6 @@ package taxi.controller.authentication;
 import java.io.IOException;
 import javax.naming.AuthenticationException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import taxi.lib.Injector;
 import taxi.model.Driver;
 import taxi.service.AuthenticationService;
 
-@WebServlet(urlPatterns = "/login")
 public class LoginController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private AuthenticationService authenticationService
@@ -21,7 +19,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/authentication/login.jsp").forward(req, resp);
     }
 
     @Override
